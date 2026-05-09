d7f8a21daad25b7ffbf4129517bc1797834a0f0e
H5N1v2 <crazybastards@web.de>
2026-05-09 08:15:18 +0200

    Extend AdapterConfig with server and widget options: Add support for NextcloudServer, including properties such as createWidget, widgetFontSize, and widgetDarkMode.

--- src/lib/adapter-config.d.ts
+++ src/lib/adapter-config.d.ts
@@ -2,12 +2,26 @@
 
 // Augment the globally declared type ioBroker.AdapterConfig
 declare global {
-	namespace ioBroker {
-		interface AdapterConfig {
-			option1: boolean;
-			option2: string;
-		}
-	}
+    namespace ioBroker {
+        interface AdapterConfig {
+            // Tab 1: Connection Settings
+            servers: NextcloudServer[];
+            
+            // Tab 2: Data Options
+            interval: number;
+            skipApps: boolean;
+            skipUpdate: boolean;
+        }
+e
+        interface NextcloudServer {
+            name: string;
+            domain: string;
+            token: string;
+            createWidget?: boolean;
+            widgetFontSize?: number;
+            widgetDarkMode?: boolean;
+        }
+    }
 }
 
 // this is required so the above AdapterConfig is found by TypeScript / type checking
